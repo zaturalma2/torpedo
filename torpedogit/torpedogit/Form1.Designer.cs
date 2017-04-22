033f452048af32ab928e8dfb3321e9d8fa4926c1
suviking <d.suviking@gmail.com>
2017-04-22 11:51:23 +0200

    asd

--- torpedogit/torpedogit/Form1.Designer.cs
+++ torpedogit/torpedogit/Form1.Designer.cs
@@ -28,12 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.table1 = new System.Windows.Forms.PictureBox();
+            this.table2 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbN = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ship2 = new System.Windows.Forms.TextBox();
+            this.ship3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ship4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ship5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.table1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // table1
+            // 
+            this.table1.Location = new System.Drawing.Point(0, 0);
+            this.table1.Name = "table1";
+            this.table1.Size = new System.Drawing.Size(250, 250);
+            this.table1.TabIndex = 0;
+            this.table1.TabStop = false;
+            // 
+            // table2
+            // 
+            this.table2.Location = new System.Drawing.Point(0, 283);
+            this.table2.Name = "table2";
+            this.table2.Size = new System.Drawing.Size(250, 250);
+            this.table2.TabIndex = 1;
+            this.table2.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(20, 34);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(200, 187);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tbN);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(192, 60);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.ship5);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.ship4);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.ship3);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.ship2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 161);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N =";
+            // 
+            // tbN
+            // 
+            this.tbN.Location = new System.Drawing.Point(36, 6);
+            this.tbN.MaxLength = 10;
+            this.tbN.Name = "tbN";
+            this.tbN.Size = new System.Drawing.Size(24, 20);
+            this.tbN.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tabControl1);
+            this.groupBox1.Location = new System.Drawing.Point(299, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 323);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "2-es hajó:";
+            // 
+            // ship2
+            // 
+            this.ship2.Location = new System.Drawing.Point(65, 9);
+            this.ship2.MaxLength = 1;
+            this.ship2.Name = "ship2";
+            this.ship2.Size = new System.Drawing.Size(23, 20);
+            this.ship2.TabIndex = 1;
+            this.ship2.Text = "1";
+            // 
+            // ship3
+            // 
+            this.ship3.Location = new System.Drawing.Point(65, 37);
+            this.ship3.MaxLength = 1;
+            this.ship3.Name = "ship3";
+            this.ship3.Size = new System.Drawing.Size(23, 20);
+            this.ship3.TabIndex = 3;
+            this.ship3.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3-es hajó:";
+            // 
+            // ship4
+            // 
+            this.ship4.Location = new System.Drawing.Point(65, 66);
+            this.ship4.MaxLength = 1;
+            this.ship4.Name = "ship4";
+            this.ship4.Size = new System.Drawing.Size(23, 20);
+            this.ship4.TabIndex = 5;
+            this.ship4.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "4-es hajó:";
+            // 
+            // ship5
+            // 
+            this.ship5.Location = new System.Drawing.Point(65, 95);
+            this.ship5.MaxLength = 1;
+            this.ship5.Name = "ship5";
+            this.ship5.Size = new System.Drawing.Size(23, 20);
+            this.ship5.TabIndex = 7;
+            this.ship5.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "5-es hajó:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 545);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.table2);
+            this.Controls.Add(this.table1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.table1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox table1;
+        private System.Windows.Forms.PictureBox table2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox tbN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox ship5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ship4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ship3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ship2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
